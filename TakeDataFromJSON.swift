ee8fdb2d320a9e5acac92d282a5ee3f47a6bdc8a
Cané <idevapps19@gmail.com>
2021-03-27 11:26:07 +0100

    Upgrade Bundle Extension

--- TakeDataFromJSON.swift
+++ TakeDataFromJSON.swift
@@ -8,21 +8,21 @@
 import Foundation
 
 extension Bundle {
-    func decode(_ file:String) -> [Recetas] {
-                guard let url = self.url( forResource: file, withExtension: nil) else {
-                    fatalError("Failed to locate \(file) in bundle.")
-                }
-
-                guard let data = try? Data(contentsOf: url) else {
-                    fatalError("Failed to load \(file) from bundle.")
-                }
-
-                let decoder = JSONDecoder()
-
-                guard let loaded = try? decoder.decode([Recetas].self, from: data) else {
-                    fatalError("Failed to decode \(file) from bundle.")
-                }
-
-                return loaded
+    func decode<T:Decodable>(_ file:String) -> [T] {
+        guard let url = self.url( forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let loaded = try? decoder.decode([T].self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+        
+        return loaded
     }
 }
